8d1aec000cdc28811ca5c8859d2d7049fb78ad1b
owens-hub-git <owencostin@bigpond.com>
2025-03-14 11:21:19 +1100

    try 2 on wednessday to match the convertion setup in lecture

--- app/src/main/java/com/example/firstandroid/MainActivity.java
+++ app/src/main/java/com/example/firstandroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,15 +19,16 @@ public class MainActivity extends AppCompatActivity {
     Button buttonConvert;
     TextView resultView;
 
-    double celcius;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     //   EdgeToEdge.enable(this);
-
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         celciusInput = findViewById(R.id.celciusInput);
         buttonConvert = findViewById(R.id.buttonConvert);
@@ -34,26 +36,22 @@ public class MainActivity extends AppCompatActivity {
 
         buttonConvert.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                String inputStr = celciusInput.getText().toString();
+            public void onClick(View v) {
 
-                if(inputStr.isEmpty()){
-                    resultView.setText("Enter a value");
-                }
+                String inputString = celciusInput.getText().toString();
 
-                try{
-                    celcius = Double.parseDouble(inputStr);
-                }
-                catch (NumberFormatException e){
-                    resultView.setText("Invalid Number");
+//                    if (inputString.isEmpty()) {
+//                    resultText.setText("Please enter some value!");
+//                  }
+//                  Toast.makeText(MainActivity.this, inputString, Toast.LENGTH_SHORT).show();
 
-                }
-                //conversion logics
-                double fahrenheit = (celcius * 1.8) + 32;
+                    double celcius = Double.parseDouble(inputString);
 
-                resultView.setText(String.format("Result: %.2f F", fahrenheit));
+                    //conversion logics
+                    double fahrenheit = (celcius * 1.8) + 32;
+
+                    resultView.setText(String.format("Result: %.2f F", fahrenheit));
             }
         });
-
     }
 }
